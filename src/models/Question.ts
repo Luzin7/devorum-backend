c41a7693f0f47a92dac8975c7ab1c872eeb78bac
Luan Victor <lvictor.dutra@gmail.com>
2023-08-31 14:44:47 -0300

    refact(Question.ts): melhora estrutura e ajusta identação

--- src/models/Question.ts
+++ src/models/Question.ts
@@ -3,16 +3,21 @@ class Questions {
   author: string;
   date: number;
   upvotes: number;
-  question: [{}];
+  question: object[];
 
-  constructor(id: number, author: string, date: number,
-    upvotes: number, comments: [{}]) {
-      this.id = id,
-      this.author = author,
-      this.date = date,
-      this.upvotes = upvotes,
-      this.question = [{}]
+  constructor(
+    id: number,
+    author: string,
+    date: number,
+    upvotes: number,
+    question: object[],
+  ) {
+    this.id = id;
+    this.author = author;
+    this.date = date;
+    this.upvotes = upvotes;
+    this.question = question;
   }
-};
+}
 
 export default Questions;
