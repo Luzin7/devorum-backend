d878e338b9eecba88c55a476d482a7fdfb796a46
Luan Victor <lvictor.dutra@gmail.com>
2023-09-05 18:14:52 -0300

    refact(UserController.ts): importa lógica e retira códigos inúteis

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,63 +1,13 @@
 import { Request, Response } from 'express';
-import { writeFile, readFile } from 'fs/promises';
-import { v4 as uuidv4 } from 'uuid';
-
-interface UsersProps {
-  date: number;
-  id: string;
-  name: string;
-  password: string;
-  contact: string;
-  question: string[];
-}
+import * as services from '../services';
+import handleError from '../functions/handleError';
 
 const registerUsers = async (req: Request, res: Response): Promise<void> => {
-  const { name, password, contact } = req.body;
-
-  const notFilled = !name || !password || !contact;
-
-  if (notFilled) {
-    res.status(409).json({ message: 'not filled' });
-  }
-
   try {
-    const usersData = await readFile('./src/data/users.json', 'utf-8');
-    const parseUsers = JSON.parse(usersData);
-
-    console.log(parseUsers);
-
-    const userAlreadyExists = parseUsers.find((user: UsersProps) => {
-      return user.name === name || user.contact === contact;
-    });
-
-    if (userAlreadyExists) {
-      res.status(409).json({ message: 'Name user already exists' });
-    }
-
-    const currentTime = new Date().getTime();
-    const newUserID = uuidv4();
-
-    const newUser = {
-      date: currentTime,
-      id: newUserID,
-      name,
-      password,
-      contact,
-      question: [],
-    };
-
-    parseUsers.push(newUser);
-
-    await writeFile(
-      './src/data/users.json',
-      JSON.stringify(parseUsers, null, 2),
-      { encoding: 'utf-8' },
-    );
-
-    res.status(201).json(newUser);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Internal server error.' });
+    await services.registerNewUser(req.body);
+    res.status(201).json();
+  } catch (error) {
+    handleError(res, error);
   }
 };
 
