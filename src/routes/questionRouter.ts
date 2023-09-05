4bfd47a7a3c74d016a9f4f8b270512cf2af981b0
Luan Victor <lvictor.dutra@gmail.com>
2023-09-05 18:20:46 -0300

    refact(questionRouter.ts): implementa middleware e remove códigos inúteis

--- src/routes/questionRouter.ts
+++ src/routes/questionRouter.ts
@@ -1,14 +1,9 @@
-import { Request, Response } from 'express';
-
 import { Router } from 'express';
 import askQuestion from '../controllers/QuestionController';
+import { validateNewQuestionRegistration } from '../middlewares';
 
 const questionRouter = Router();
 
-questionRouter.get('/questions', (req: Request, res: Response) => {
-  res.send('All Questions');
-});
-
-questionRouter.post('/questions', askQuestion);
+questionRouter.post('/questions', validateNewQuestionRegistration, askQuestion);
 
 export default questionRouter;
