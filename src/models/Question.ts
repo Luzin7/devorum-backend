2d669a139166689d0726ab805de0adb56109437c
Luan Victor <lvictor.dutra@gmail.com>
2023-09-08 10:38:14 -0300

    feat(Question.ts): adicionar id do autor da pergunta

--- src/models/Question.ts
+++ src/models/Question.ts
@@ -1,6 +1,7 @@
 class Question {
   date: number;
   id: string;
+  authorId: string;
   author: string;
   question: string;
   upvotes: number;
@@ -9,12 +10,14 @@ class Question {
   constructor(
     date: number,
     id: string,
+    authorId: string,
     author: string,
     question: string,
     upvotes: number,
   ) {
     this.date = date;
     this.id = id;
+    this.authorId = authorId;
     this.author = author;
     this.question = question;
     this.upvotes = upvotes;
