7e8ad51dc0afb035ed3d195c27393b5fe16cdc03
Luan Victor <lvictor.dutra@gmail.com>
2023-09-09 01:56:55 -0300

    feat(QuestionController.ts): implementa funções de delete e get

--- src/controllers/QuestionController.ts
+++ src/controllers/QuestionController.ts
@@ -2,16 +2,25 @@ import { Request, Response } from 'express';
 import * as services from '../services';
 import handleError from '../functions/handleError';
 
-const askQuestion = async (req: Request, res: Response): Promise<void> => {
+const getQuestions = async (req: Request, res: Response): Promise<void> => {
   try {
-    await services.registerNewQuestion(req.body);
+    const questions = await services.getQuestions();
+    res.json(questions);
+  } catch (error) {
+    handleError(res, error);
+  }
+};
+
+const createQuestion = async (req: Request, res: Response): Promise<void> => {
+  try {
+    await services.createQuestion(req.body);
     res.status(201).json();
   } catch (error) {
     handleError(res, error);
   }
 };
 
-const addComment = async (req: Request, res: Response): Promise<void> => {
+const createComment = async (req: Request, res: Response): Promise<void> => {
   try {
     await services.addNewComment(req.body);
     res.status(201).json();
@@ -20,4 +29,31 @@ const addComment = async (req: Request, res: Response): Promise<void> => {
   }
 };
 
-export { askQuestion, addComment };
+const deleteQuestion = async (req: Request, res: Response): Promise<void> => {
+  const { questionId } = req.params;
+
+  try {
+    await services.deleteQuestion(questionId);
+    res.status(202).json();
+  } catch (error) {
+    handleError(res, error);
+  }
+};
+
+const deleteComment = async (req: Request, res: Response): Promise<void> => {
+  const { questionId, commentId } = req.params;
+  try {
+    await services.deleteComment(commentId, questionId);
+    res.status(202).json();
+  } catch (error) {
+    handleError(res, error);
+  }
+};
+
+export {
+  getQuestions,
+  createQuestion,
+  createComment,
+  deleteQuestion,
+  deleteComment,
+};
