aac4b54264174a3bed222b54afa7e451676caade
Luan Victor <lvictor.dutra@gmail.com>
2023-09-11 19:43:07 -0300

    remove

--- src/routes/changePasswordRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import changeUserPassword from '../controllers/ChangeController';
-
-const changePass = Router();
-
-changePass.put('/change-password/:userId', changeUserPassword);
-
-export default changePass;
