5dd4f6a5657284892795a2956e05e6679a6231e5
Luan Victor <lvictor.dutra@gmail.com>
2023-09-11 18:13:03 -0300

    em andamento

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import app from './app';
 
 const { PORT } = require('../config');
 
-const port = PORT || '4422';
+const port = PORT || '4425';
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
