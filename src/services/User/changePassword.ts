93da27ade9a174030036c83256bf1e7cb9a5b7db
Luan Victor <lvictor.dutra@gmail.com>
2023-09-15 18:15:58 -0300

    bomb commit

--- src/services/User/changePassword.ts
+++ src/services/User/changePassword.ts
@@ -11,34 +11,30 @@ const changeUserPassword = async ({
   newPassword,
   newPasswordConfirm,
 }: ChangePasswordProps): Promise<void> => {
-  try {
-    const userExists = await database.existingUser(userId);
+  const userExists = await database.existingUser(userId);
 
-    if (userExists.length <= 0) {
-      throw new Error('User not found');
-    }
-    const user = userExists[0];
-
-    if (user.password !== currentPassword) {
-      throw new Error('Current password is incorrect');
-    }
-
-    if (newPassword !== newPasswordConfirm) {
-      throw new Error('New passwords do not match');
-    }
-
-    if (newPassword.length < 6 || newPassword.length > 16) {
-      throw new Error('New password must have at least 6 or 16 characters');
-    }
-
-    const salt = crypto.randomBytes(16).toString('hex');
-
-    const encryptedNewPassword = encryptPassword(newPassword, salt);
-
-    database.updatePassword(userId, encryptedNewPassword, salt);
-  } catch (error) {
-    throw new Error(error);
+  if (userExists.length <= 0) {
+    throw new Error('User not found');
   }
+  const user = userExists[0];
+
+  if (user.password !== currentPassword) {
+    throw new Error('Current password is incorrect');
+  }
+
+  if (newPassword !== newPasswordConfirm) {
+    throw new Error('New passwords do not match');
+  }
+
+  if (newPassword.length < 6 || newPassword.length > 16) {
+    throw new Error('New password must have at least 6 or 16 characters');
+  }
+
+  const salt = crypto.randomBytes(16).toString('hex');
+
+  const encryptedNewPassword = encryptPassword(newPassword, salt);
+
+  database.updatePassword(userId, encryptedNewPassword, salt);
 };
 
 export default changeUserPassword;
--- src/services/User/login.ts
+++ src/services/User/login.ts
@@ -6,31 +6,27 @@ import { Row } from 'postgres';
 const database = new UsersDbPg();
 
 const login = async ({ name, password }: User): Promise<Row> => {
-  try {
-    const userExists = await database.existingUser(name);
+  const userExists = await database.existingUser(name);
 
-    if (userExists.length <= 0) {
-      throw new Error('User does not exist');
-    }
-
-    const isPasswordValid =
-      userExists[0].password === encryptPassword(password, userExists[0].salt);
-
-    if (!isPasswordValid) {
-      throw new Error('Invalid password');
-    }
-
-    const user = {
-      id: userExists[0].id,
-      name: userExists[0].name,
-      contact: userExists[0].contact,
-      questions: userExists[0].questions,
-    };
-
-    return user;
-  } catch (error) {
-    throw new Error(error);
+  if (userExists.length <= 0) {
+    throw new Error('User does not exist');
   }
+
+  const isPasswordValid =
+    userExists[0].password === encryptPassword(password, userExists[0].salt);
+
+  if (!isPasswordValid) {
+    throw new Error('Invalid password');
+  }
+
+  const user = {
+    id: userExists[0].id,
+    name: userExists[0].name,
+    contact: userExists[0].contact,
+    questions: userExists[0].questions,
+  };
+
+  return user;
 };
 
 export default login;
