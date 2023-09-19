58589bcb550cb3575687647ed826fadfa0043a82
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-09-18 23:24:57 -0300

    feat(index.ts): adding create user service

---
+++ src/module/users/services/createUserService/index.ts
@@ -0,0 +1,51 @@
+import { User } from '@module/users/entities/User'
+import { UserAlreadyExitesError } from '@module/users/errors/UserAlreadyExitesError'
+import { UsersRepository } from '@module/users/repositories/contracts/UsersRepository'
+import { Either, left, right } from '@shared/core/errors/Either'
+import { pbkdf2Sync, randomBytes } from 'crypto'
+
+function encryptPassword(password: string, salt: string): string {
+  const hash = pbkdf2Sync(password, salt, 10000, 64, 'sha256').toString('hex')
+  return hash
+}
+
+interface Request {
+  name: string
+  password: string
+  email: string
+}
+
+type Response = Either<
+  UserAlreadyExitesError,
+  {
+    user: User
+  }
+>
+
+export class CreateUserService {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  async execute({ email, name, password }: Request): Promise<Response> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email)
+
+    if (userAlreadyExists) {
+      return left(new UserAlreadyExitesError())
+    }
+
+    const salt = randomBytes(16).toString('hex')
+    const encryptedPassword = encryptPassword(password, salt)
+
+    const user = User.create({
+      email,
+      name,
+      password: encryptedPassword,
+      salt,
+    })
+
+    await this.usersRepository.create(user)
+
+    return right({
+      user,
+    })
+  }
+}
