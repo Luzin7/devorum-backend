68de69a29c8c5728ca58080f625b65a8953f66b7
Luan Victor <lvictor.dutra@gmail.com>
2023-09-18 22:03:18 -0300

    feat(index.ts): add getter

--- src/shared/core/entities/Entity/index.ts
+++ src/shared/core/entities/Entity/index.ts
@@ -1,11 +1,15 @@
 import { UniqueId } from '../UniqueId'
 
 export class Entity<T> {
-  private id: UniqueId
-  private props: T
+  private _id: UniqueId
+  protected props: T
 
   protected constructor(props: T, id?: UniqueId) {
     this.props = props
-    this.id = id ?? new UniqueId()
+    this._id = id ?? new UniqueId()
+  }
+
+  get id(): UniqueId {
+    return this._id
   }
 }
