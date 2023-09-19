311a2e6948502c62bb9d09d3b7ba4d382b3e0350
Luan Victor <lvictor.dutra@gmail.com>
2023-09-18 22:01:36 -0300

    test(vite.config.ts): configura teste

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { configDefaults, defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    includeSource: ['src/**/*.spec.ts'],
+    exclude: [...configDefaults.exclude, 'src/**/*.e2e.spec.ts'],
+    globals: true,
+  },
+  resolve: {
+    alias: {
+      '@module/': '/src/module/',
+      '@shared/': '/src/shared/',
+    },
+  },
+})
