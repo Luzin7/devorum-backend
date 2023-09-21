c4a43664540386874c55251189bca951b270c97a
Luan Victor <lvictor.dutra@gmail.com>
2023-09-20 21:14:00 -0300

    test(makeTopic.ts): adiciona fabrica de topicos

---
+++ src/test/module/topic/factories/makeTopic.ts
@@ -0,0 +1,18 @@
+import { fakerPT_BR } from '@faker-js/faker'
+import { Topic, TopicProps } from '@module/topics/entities/Topic'
+import { UniqueId } from '@shared/core/entities/UniqueId'
+
+export function makeTopic(override: Partial<TopicProps> = {}, id?: UniqueId) {
+  const topic = Topic.create(
+    {
+      authorId: new UniqueId(fakerPT_BR.string.uuid()),
+      authorName: fakerPT_BR.person.fullName(),
+      content: fakerPT_BR.lorem.sentence(5),
+      title: fakerPT_BR.lorem.words(8),
+      ...override,
+    },
+    id,
+  )
+
+  return topic
+}
