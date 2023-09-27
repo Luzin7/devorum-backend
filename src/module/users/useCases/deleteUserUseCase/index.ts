5b15f651cce877b9ddc4458a92202abc9de82457
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-09-27 18:25:11 -0300

    Feat: Adiciona inversão de dependencias

--- src/module/users/useCases/deleteUserUseCase/index.ts
+++ src/module/users/useCases/deleteUserUseCase/index.ts
@@ -1,6 +1,8 @@
+import { Injectable } from '@infra/containers/Injectable'
 import { UserNotFoundError } from '@module/users/errors/UserNotFoundError'
 import { UsersRepository } from '@module/users/repositories/contracts/UsersRepository'
 import { Either, left, right } from '@shared/core/errors/Either'
+import { inject, injectable } from 'tsyringe'
 
 interface Request {
   id: string
@@ -8,8 +10,12 @@ interface Request {
 
 type Response = Either<UserNotFoundError, null>
 
+@injectable()
 export class DeleteUserUseCase {
-  constructor(private readonly usersRepository: UsersRepository) {}
+  constructor(
+    @inject(Injectable.Repositories.Users)
+    private readonly usersRepository: UsersRepository,
+  ) {}
 
   async execute({ id }: Request): Promise<Response> {
     const user = await this.usersRepository.findById(id)
