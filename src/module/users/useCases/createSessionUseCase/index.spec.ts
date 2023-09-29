b53d0aec93d47d67be65e82b553e60221ac740e5
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-09-29 18:48:24 -0300

    Feat: Adiciona caso de uso para criar sessão

---
+++ src/module/users/useCases/createSessionUseCase/index.spec.ts
@@ -0,0 +1,83 @@
+import 'reflect-metadata'
+import { UsersInMemoryRepository } from '@test/module/user/repositories/UsersInMemoryRepository'
+import { makeUser } from '@test/module/user/factories/makeUser'
+import { FakeCryptographyProvider } from '@test/providers/cryptography/fakeCryptographyProvider'
+import { CreateSessionUseCase } from '.'
+import { FakeAuthProvider } from '@test/providers/auth/fakeAuthProvider'
+import { WrongCredentialsError } from '@module/users/errors/WrongCredentialsError'
+
+let usersRepository: UsersInMemoryRepository
+
+let cryptographyProvider: FakeCryptographyProvider
+
+let authProvider: FakeAuthProvider
+let sut: CreateSessionUseCase
+
+describe('create session', () => {
+  beforeEach(() => {
+    usersRepository = new UsersInMemoryRepository()
+
+    cryptographyProvider = new FakeCryptographyProvider()
+    authProvider = new FakeAuthProvider()
+
+    sut = new CreateSessionUseCase(
+      cryptographyProvider,
+      usersRepository,
+      authProvider,
+    )
+  })
+
+  it('should be able to create an new session for user', async () => {
+    const { hash, salt } = await cryptographyProvider.hashCreator('12345678')
+
+    const user = makeUser({
+      password: hash,
+      salt,
+      email: 'test@test.com',
+    })
+
+    await usersRepository.create(user)
+
+    const response = await sut.execute({
+      email: 'test@test.com',
+      password: '12345678',
+    })
+
+    expect(response.isRight()).toEqual(true)
+    expect(usersRepository.users[0].password).not.equal('password')
+
+    if (response.isRight()) {
+      expect(response.value.accessToken).toEqual(`${user.id.toString()}-access`)
+    }
+  })
+
+  it('not should be able to create an new session for user if pass is wrong', async () => {
+    const { hash, salt } = await cryptographyProvider.hashCreator('12345678')
+
+    const user = makeUser({
+      password: hash,
+      salt,
+      email: 'test@test.com',
+    })
+
+    await usersRepository.create(user)
+
+    const response = await sut.execute({
+      email: 'test@test.com',
+      password: 'wrong-password',
+    })
+
+    expect(response.isLeft()).toEqual(true)
+    expect(response.value).toBeInstanceOf(WrongCredentialsError)
+  })
+
+  it('not should be able to create an new session for user if user not exist', async () => {
+    const response = await sut.execute({
+      email: 'unixistent@user.com',
+      password: '12345678',
+    })
+
+    expect(response.isLeft()).toEqual(true)
+    expect(response.value).toBeInstanceOf(WrongCredentialsError)
+  })
+})
