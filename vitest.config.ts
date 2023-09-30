13dbcce2e776df0cc817b6fdd9ad31a409e0d7ca
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-09-30 16:57:48 -0300

    Test: Atualiza exclude de test

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { configDefaults, defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     includeSource: ['src/**/*.spec.ts'],
-    exclude: [...configDefaults.exclude, 'data/**/*', 'src/**/*.e2e.spec.ts'],
+    exclude: [...configDefaults.exclude, '.data', 'src/**/*.e2e.spec.ts'],
     globals: true,
   },
   resolve: {
