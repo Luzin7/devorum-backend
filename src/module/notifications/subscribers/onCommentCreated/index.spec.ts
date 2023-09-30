5625be8ff991580b3b16b2fb1c4432229f4b6bb6
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-09-30 02:47:48 -0300

    Test: Cria teste para evento de criação de um comentário criado

---
+++ src/module/notifications/subscribers/onCommentCreated/index.spec.ts
@@ -0,0 +1,51 @@
+import 'reflect-metadata'
+import { makeComment } from '@test/module/comment/factories/makeComment'
+import { OnCommentCreated } from '.'
+import { CommentsInMemoryRepository } from '@test/module/comment/repositories/CommentsInMemoryRepository'
+import { TopicsInMemoryRepository } from '@test/module/topic/repositories/TopicsInMemoryRepository'
+import { UsersInMemoryRepository } from '@test/module/user/repositories/UsersInMemoryRepository'
+import { SendNotificationUseCase } from '@module/notifications/useCases/sendNotificationUseCase'
+import { NotificationsInMemoryRepository } from '@test/module/notification/repositories/NotificationsInMemory'
+import { makeTopic } from '@test/module/topic/factories/makeTopic'
+import { SpyInstance } from 'vitest'
+import { waitFor } from '@test/utils/waitFor'
+
+let usersRepository: UsersInMemoryRepository
+let topicsRepository: TopicsInMemoryRepository
+let commentsRepository: CommentsInMemoryRepository
+let notificationsRepository: NotificationsInMemoryRepository
+
+let sendNotificationUseCase: SendNotificationUseCase
+
+let sendNotificationExecuteSpy: SpyInstance
+
+describe('On comment created', () => {
+  beforeEach(() => {
+    usersRepository = new UsersInMemoryRepository()
+    commentsRepository = new CommentsInMemoryRepository()
+    topicsRepository = new TopicsInMemoryRepository(usersRepository)
+    notificationsRepository = new NotificationsInMemoryRepository()
+
+    sendNotificationUseCase = new SendNotificationUseCase(
+      notificationsRepository,
+    )
+
+    sendNotificationExecuteSpy = vi.spyOn(sendNotificationUseCase, 'execute')
+
+    new OnCommentCreated(topicsRepository, sendNotificationUseCase)
+  })
+
+  it('should send a notification when an comment is created', async () => {
+    const topic = makeTopic()
+    const comment = makeComment({
+      topicId: topic.id,
+    })
+
+    topicsRepository.create(topic)
+    commentsRepository.create(comment)
+
+    await waitFor(() => {
+      expect(sendNotificationExecuteSpy).toHaveBeenCalled()
+    })
+  })
+})
