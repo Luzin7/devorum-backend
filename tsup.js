a7740ddde3c7f1f08c82187d4afd16dc6f88958e
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-09-30 16:44:03 -0300

    Build: Atualiza estratégia de build

--- tsup.js
+++ tsup.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'tsup'
-import multiEntry from 'rollup-plugin-multi-entry'
+import multiEntry from '@rollup/plugin-multi-entry'
 
 export default defineConfig({
   entry: ['src/module/**/*', 'src/shared/**/*'],
-  format: ['cjs', 'esm'],
-  plugins: [multiEntry()],
+  format: ['esm'],
+  tsconfig: 'tsconfig.json',
+  target: 'es2021',
+  plugins: [multiEntry({
+    exclude: 'node_modules',
+  })],
   outDir: './dist',
 })
