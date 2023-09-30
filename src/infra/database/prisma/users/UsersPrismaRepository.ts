87474424bc557430e7b3df1a17f90b386c1cbb4e
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-09-30 02:44:03 -0300

    Feat: Adiciona busca de usuário com as nitificações

--- src/infra/database/prisma/users/UsersPrismaRepository.ts
+++ src/infra/database/prisma/users/UsersPrismaRepository.ts
@@ -2,6 +2,8 @@ import { prisma } from '@infra/database/createConnection'
 import { User } from '@module/users/entities/User'
 import { UsersRepository } from '@module/users/repositories/contracts/UsersRepository'
 import { UsersPrismaMapper } from './UsersPrismaMapper'
+import { UserWithNotifications } from '@module/users/valueObjects/UserWithNotifications'
+import { UsersWithNotificationsPrismaMapper } from './UsersWithNotificationsPrismaMapper'
 
 export class UsersPrismaRepository implements UsersRepository {
   async create(user: User): Promise<void> {
@@ -22,6 +24,28 @@ export class UsersPrismaRepository implements UsersRepository {
     }
 
     return UsersPrismaMapper.toEntity(user)
+  }
+
+  async findByIdWithNotifications(
+    id: string,
+  ): Promise<UserWithNotifications | null> {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        notifications: {
+          orderBy: {
+            createdAt: 'desc',
+          },
+          take: 50,
+        },
+      },
+    })
+
+    if (!user) return null
+
+    return UsersWithNotificationsPrismaMapper.toEntity(user)
   }
 
   async findByEmail(email: string): Promise<User | null> {
