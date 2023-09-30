503f0ff6958835e598638af96ee5a05134fbbd64
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-09-30 02:50:33 -0300

    Feat: Adiciona controle de eventos nos agregados

--- src/shared/core/entities/AggregateRoot/index.ts
+++ src/shared/core/entities/AggregateRoot/index.ts
@@ -1,3 +1,20 @@
+import { DomainEvent } from '@shared/core/events/DomainEvent'
 import { Entity } from '../Entity'
+import { DomainEvents } from '@shared/core/events/DomainEvents'
 
-export abstract class AggregateRoot<Props> extends Entity<Props> {}
+export abstract class AggregateRoot<Props> extends Entity<Props> {
+  private _domainEvents: DomainEvent[] = []
+
+  get domainEvents() {
+    return this._domainEvents
+  }
+
+  protected addDomainEvent(domainEvent: DomainEvent): void {
+    this._domainEvents.push(domainEvent)
+    DomainEvents.markAggregateForDispatch(this)
+  }
+
+  public clearEvents() {
+    this._domainEvents = []
+  }
+}
