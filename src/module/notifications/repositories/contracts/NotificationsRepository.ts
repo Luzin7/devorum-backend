f6d1a9566e3a972d3cccd592a7680ac5508eb04f
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-09-30 02:46:51 -0300

    Feat: Adiciona abstração de metodos do repositório de notificação

--- src/module/notifications/repositories/contracts/NotificationsRepository.ts
+++ src/module/notifications/repositories/contracts/NotificationsRepository.ts
@@ -2,4 +2,6 @@ import { Notification } from '@module/notifications/entities/Notification'
 
 export abstract class NotificationsRepository {
   abstract create(notification: Notification): Promise<void>
+  abstract findById(id: string): Promise<Notification | null>
+  abstract save(notification: Notification): Promise<void>
 }
