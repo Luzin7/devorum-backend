7e48f2d996683ba84f093c3e4f29a5a591cd0368
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-09-30 02:42:33 -0300

    Teste: Adiciona metodos pendentes no repositório de teste

--- src/test/module/notification/repositories/NotificationsInMemory.ts
+++ src/test/module/notification/repositories/NotificationsInMemory.ts
@@ -9,4 +9,26 @@ export class NotificationsInMemoryRepository
   async create(notification: Notification): Promise<void> {
     this.notifications.push(notification)
   }
+
+  async findById(id: string): Promise<Notification | null> {
+    const notification = this.notifications.find(
+      (notification) => notification.id.toString() === id,
+    )
+
+    if (!notification) return null
+
+    return notification
+  }
+
+  async save(notification: Notification): Promise<void> {
+    const notificationIndex = this.notifications.findIndex((n) =>
+      n.id.equals(notification.id),
+    )
+
+    if (notificationIndex < 0) {
+      throw new Error('Notification not created')
+    }
+
+    this.notifications[notificationIndex] = notification
+  }
 }
