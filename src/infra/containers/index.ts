2e623bfe5a7f5d1658a1778fcccfb7eaa7bc0a63
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-09-30 02:40:13 -0300

    Feat: Registra repositórios necessários para notifiações

--- src/infra/containers/index.ts
+++ src/infra/containers/index.ts
@@ -1,5 +1,7 @@
 import '@infra/containers/modules/users'
 import '@infra/containers/modules/comments'
 import '@infra/containers/modules/topics'
+import '@infra/containers/modules/notifications'
 import '@infra/containers/providers/cryptography'
 import '@infra/containers/providers/auth'
+import '@infra/containers/events/comments'
