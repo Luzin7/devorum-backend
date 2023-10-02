5ff3897caaa95f1deeceb829075056ffd514cca3
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-10-02 18:16:40 -0300

    Feat: Registra rota de atulização de comentário

--- src/infra/http/routes/comments/index.ts
+++ src/infra/http/routes/comments/index.ts
@@ -1,9 +1,11 @@
 import { authMiddleware } from '@infra/http/middlewares/authMiddleware'
 import { CreateCommentController } from '@module/comments/controllers/createCommentController'
 import { DeleteCommentController } from '@module/comments/controllers/deleteCommentController'
+import { UpdateCommentController } from '@module/comments/controllers/updateCommentController'
 import { Router } from 'express'
 
 const createCommentController = new CreateCommentController()
+const updateCommentController = new UpdateCommentController()
 const deleteCommentController = new DeleteCommentController()
 
 const commentsRoutes = Router()
@@ -13,6 +15,13 @@ commentsRoutes.post(
   authMiddleware.middle,
   createCommentController.handle,
 )
+
+commentsRoutes.patch(
+  '/comments/:commentId',
+  authMiddleware.middle,
+  updateCommentController.handle,
+)
+
 commentsRoutes.delete(
   '/topics/:topicId/comments/:commentId',
   authMiddleware.middle,
