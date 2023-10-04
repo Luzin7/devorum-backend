70846275c0eb15c8db8df5bf1f2038a0c4d9beb0
Stormlig <114263838+Stormlig@users.noreply.github.com>
2023-10-04 20:11:45 -0300

    feat: adiciona o provider de date

---
+++ src/providers/date/contracts/DateProvider.ts
@@ -0,0 +1,9 @@
+interface IsBeforeProps {
+  initialDate: Date
+  endDate: Date
+}
+
+export abstract class DateProvider {
+  abstract addDays(days: number): Date
+  abstract isBefore(props: IsBeforeProps): boolean
+}
