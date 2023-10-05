71809e230668de2615cbcaf4862c0ca4df4c79f8
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-10-05 18:14:42 -0300

    Test: Fix test error

--- src/test/module/user/repositories/RefreshTokensInMemoryRepository.ts
+++ src/test/module/user/repositories/RefreshTokensInMemoryRepository.ts
@@ -1,5 +1,8 @@
 import { RefreshToken } from '@module/users/entities/RefreshToken'
-import { RefreshTokensRepository } from '@module/users/repositories/contracts/RefreshTokensRepository'
+import {
+  FindByUserIdAndRefreshTokenProps,
+  RefreshTokensRepository,
+} from '@module/users/repositories/contracts/RefreshTokensRepository'
 
 export class RefreshTokensInMemoryRepository
   implements RefreshTokensRepository
@@ -8,5 +11,27 @@ export class RefreshTokensInMemoryRepository
 
   async create(refreshToken: RefreshToken): Promise<void> {
     this.refreshTokens.push(refreshToken)
+  }
+
+  async findByUserIdAndRefreshToken({
+    refreshToken,
+    userId,
+  }: FindByUserIdAndRefreshTokenProps): Promise<RefreshToken | null> {
+    const refreshTokenSaved = this.refreshTokens.find(
+      (rt) =>
+        rt.refreshToken === refreshToken && rt.usedId.toString() === userId,
+    )
+
+    if (!refreshTokenSaved) {
+      return null
+    }
+
+    return refreshTokenSaved
+  }
+
+  async delete(id: string): Promise<void> {
+    this.refreshTokens = this.refreshTokens.filter(
+      (refreshToken) => refreshToken.id.toString() !== id,
+    )
   }
 }
