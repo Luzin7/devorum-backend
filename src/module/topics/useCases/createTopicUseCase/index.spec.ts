887a3a3eba44287db139c22e23213b892007b653
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-10-26 21:30:31 -0300

    [Refactor]: Remove log and fix test

--- src/module/topics/useCases/createTopicUseCase/index.spec.ts
+++ src/module/topics/useCases/createTopicUseCase/index.spec.ts
@@ -6,10 +6,13 @@ import { makeUser } from '@test/module/user/factories/makeUser'
 import { UniqueId } from '@shared/core/entities/UniqueId'
 import { UserNotFoundError } from '@module/users/errors/UserNotFoundError'
 import { NotificationsInMemoryRepository } from '@test/module/notification/repositories/NotificationsInMemory'
+import { TextProvider } from '@providers/text/contracts/TextProvider'
+import { TextImplementedProvider } from '@providers/text/implementations/TextImplementedProvider'
 
 let notificationsRepository: NotificationsInMemoryRepository
 let topicsRepository: TopicsInMemoryRepository
 let usersRepository: UsersInMemoryRepository
+let textProvider: TextProvider
 let sut: CreateTopicUseCase
 
 describe('create topic', () => {
@@ -17,8 +20,13 @@ describe('create topic', () => {
     notificationsRepository = new NotificationsInMemoryRepository()
     usersRepository = new UsersInMemoryRepository(notificationsRepository)
     topicsRepository = new TopicsInMemoryRepository(usersRepository)
+    textProvider = new TextImplementedProvider()
 
-    sut = new CreateTopicUseCase(topicsRepository, usersRepository)
+    sut = new CreateTopicUseCase(
+      topicsRepository,
+      usersRepository,
+      textProvider,
+    )
   })
 
   it('should be able to create an new topic', async () => {
