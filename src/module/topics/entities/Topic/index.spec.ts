837373850b034776acfc1636a7787b8f16177d9b
Stormlig <114263838+Stormlig@users.noreply.github.com>
2023-10-29 17:19:24 -0300

    test: add isDeleted ao teste entidade

--- src/module/topics/entities/Topic/index.spec.ts
+++ src/module/topics/entities/Topic/index.spec.ts
@@ -7,9 +7,11 @@ describe('Instance topic', () => {
       authorId: new UniqueId(),
       title: 'Titulo legal',
       content: 'Cool content',
+      isDeleted: false,
     })
     expect(topic.id).toBeInstanceOf(UniqueId)
     expect(topic.createdAt).toBeInstanceOf(Date)
     expect(topic.content).toEqual('Cool content')
+    expect(topic.isDeleted).toBe(false)
   })
 })
