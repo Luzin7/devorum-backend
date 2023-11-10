fbd14efaeca53edb480efb30ea7be6ba38517345
Stormlig <114263838+Stormlig@users.noreply.github.com>
2023-11-10 20:38:51 -0300

    feat: add route updateTopicController

--- src/infra/http/routes/topics/index.ts
+++ src/infra/http/routes/topics/index.ts
@@ -3,12 +3,14 @@ import { CreateTopicController } from '@module/topics/controllers/createTopicCon
 import { DeleteTopicController } from '@module/topics/controllers/deleteTopicController'
 import { FetchRecentTopicsController } from '@module/topics/controllers/fetchRecentTopicsController'
 import { GetTopicDetailsController } from '@module/topics/controllers/getTopicDetailsController'
+import { UpdateTopicController } from '@module/topics/controllers/updateTopicController'
 import { Router } from 'express'
 
 const createTopicController = new CreateTopicController()
 const getTopicDetailsController = new GetTopicDetailsController()
 const deleteTopicController = new DeleteTopicController()
 const fetchRecentTopicsController = new FetchRecentTopicsController()
+const updateTopicController = new UpdateTopicController()
 
 const topicsRoutes = Router()
 
@@ -21,6 +23,11 @@ topicsRoutes.post(
   '/topics',
   authMiddleware.middle,
   createTopicController.handle,
+)
+topicsRoutes.put(
+  '/topics/:topicId',
+  authMiddleware.middle,
+  updateTopicController.handle,
 )
 topicsRoutes.get('/topics', fetchRecentTopicsController.handle)
 topicsRoutes.get('/topics/:topicId', getTopicDetailsController.handle)
